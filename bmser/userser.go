72d143acdfba074a2872c78f3709f32cdd5abe3c
AlfredYang1986 <yangyuanpig@gmail.com>
2018-08-06 16:33:13 +0800

    一个查询

--- bmser/userser.go
+++ bmser/userser.go
@@ -3,8 +3,9 @@ package main
 import (
 	//"encoding/json"
 	"fmt"
-	//"github.com/alfredyang1986/blackmirror/bmmodel/brand"
+	"github.com/alfredyang1986/blackmirror/bmmodel/brand"
 	"github.com/alfredyang1986/blackmirror/bmmodel/location"
+	"github.com/alfredyang1986/blackmirror/bmmodel/request"
 	"github.com/alfredyang1986/blackmirror/jsonapi"
 	"github.com/gorilla/mux"
 	"io/ioutil"
@@ -36,6 +37,32 @@ func main() {
 		err = jsonapi.ToJsonAPI(&t, w)
 
 		//fmt.Fprintf(w, "Welcome to my website!")
+	}).Methods("POST")
+
+	r.HandleFunc("/find", func(w http.ResponseWriter, r *http.Request) {
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("Error reading body: %v", err)
+			http.Error(w, "can't read body", http.StatusBadRequest)
+			return
+		}
+		sjson := string(body)
+		fmt.Println(sjson)
+
+		rst, _ := jsonapi.FromJsonAPI(sjson)
+		println(rst)
+
+		t := rst.(request.Request)
+
+		tmp := brand.Brand{}
+		tmp.FindOne(t)
+		fmt.Println(tmp)
+
+		w.Header().Add("Content-Type", "application/json")
+		err = jsonapi.ToJsonAPI(&tmp, w)
+		fmt.Fprintf(w, "Welcome to my website!")
+
 	}).Methods("POST")
 
 	//fs := http.FileServer(http.Dir("static/"))
